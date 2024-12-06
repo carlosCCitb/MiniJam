8f11e30db6f98c0cba235d4e1c8b35672b23ac99
Victor Langa <victor.langa.delafuente@gmail.com>
2024-12-06 15:40:09 +0100

    Start with enemy pool (this is to do a merge)

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using AYellowpaper.SerializedCollections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Serializable]
+    private struct Wave
+    {
+        public SpawnData[] spawnData;
+    }
+
+    [Serializable]
+    private struct SpawnData
+    {
+        public EnemyPool.Type enemyType;
+        public EnemySO enemyData;
+        public Transform spawningTransform;
+    }
+
+    [SerializeField] private SerializedDictionary<int, Wave> _waves;
+
+    public void Spawn(int level)
+    { 
+        //if (!)
+    }
+}
