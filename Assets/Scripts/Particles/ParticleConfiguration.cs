7681b1fffc28176f913a2c8a4e3ef4de3a65e677
Victor Langa <victor.langa.delafuente@gmail.com>
2024-12-06 15:21:24 +0100

    Add hit rock particle and particle pool scripts

---
+++ Assets/Scripts/Particles/ParticleConfiguration.cs
@@ -0,0 +1,62 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ParticleConfiguration : MonoBehaviour, Pool<ParticleConfiguration, ParticleConfiguration.Type, ParticleConfiguration>.IPoolable
+{
+    public enum Type
+    { 
+        HitRock = 0
+    }
+
+    [SerializeField] private Type _type;
+    [SerializeField] private List<ParticleSystem> _particleSystems;
+
+    public Type PoolableType => _type;
+    public GameObject PoolableGameObject => gameObject;
+    
+    public event Action<ParticleConfiguration> OnPoolableDespawnNeeded;
+
+    private CancellationTokenSource _cancellationTokenSource;
+
+    private void OnEnable()
+    {
+        _cancellationTokenSource = new();
+    }
+
+    private void OnDisable()
+    {
+        _cancellationTokenSource?.Cancel();
+    }
+
+    public async UniTaskVoid OnParticleSpawned()
+    {
+        await UniTask.DelayFrame(1);
+
+        _particleSystems.ForEach(x => x.Play());
+
+        await UniTask.WaitUntil(AreParticleSystemsFinished, cancellationToken: _cancellationTokenSource.Token);
+
+        OnPoolableDespawnNeeded?.Invoke(this);
+    }
+
+    private bool AreParticleSystemsFinished()
+    {
+        if (_particleSystems.Count == 0)
+            return true;
+
+        bool shouldStop = true;
+
+        foreach (var particleSystem in _particleSystems)
+            shouldStop &= !particleSystem.isPlaying;
+
+        return shouldStop;
+    }
+
+    private void OnDestroy()
+    {
+        _cancellationTokenSource?.Dispose();
+    }
+}
