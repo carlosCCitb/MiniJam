c0bc26f45a4356401864b8cb6fc46def33a549fa
Carlos Carrasco <carlos.carrasco@itb.cat>
2024-12-07 11:47:37 +0100

    ParticleProcedural

--- Assets/Scripts/Meteorite/DragMeteoriteBehaviour.cs
+++ Assets/Scripts/Meteorite/DragMeteoriteBehaviour.cs
@@ -4,6 +4,12 @@ public class DragMeteoriteBehaviour : MonoBehaviour
 {
     public List<GameObject> MeteorSkins;
     [SerializeField] private int _currentSkin;
+    [SerializeField] private ParticleSystem particles;
+    [SerializeField] private List<float> ShapeValues;
+    private float _initialRateOverTime;
+    private ParticleSystem.ShapeModule shape;
+    private ParticleSystem.EmissionModule emission;
+    [SerializeField] private float rateMultiplier;
     private void Awake()
     {
         _currentSkin = 0;
@@ -11,6 +17,10 @@ public class DragMeteoriteBehaviour : MonoBehaviour
             meteor.SetActive(false);
         MeteorSkins[0].SetActive(true);
         MeteorSkins[5].SetActive(true);
+        shape = particles.shape;
+        emission = particles.emission;
+        shape.radius = ShapeValues[0];
+        _initialRateOverTime = emission.rateOverTime.constant;
     }
     public void ChangeSkin(int i)
     {
@@ -25,5 +35,11 @@ public class DragMeteoriteBehaviour : MonoBehaviour
         MeteorSkins[_currentSkin].SetActive(true);
         if (_currentSkin + 5 < MeteorSkins.Count)
             MeteorSkins[_currentSkin + 5].SetActive(true);
+        AdjustParticlesToShape(i);
+    }
+    public void AdjustParticlesToShape(int i)
+    {
+        shape.radius = ShapeValues[i];
+        emission.rateOverTime = _initialRateOverTime - _initialRateOverTime* rateMultiplier * i;
     }
 }
