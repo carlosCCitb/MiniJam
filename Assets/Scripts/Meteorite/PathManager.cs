7edc10be2097ccfadacf26e3d98cb3847b6f9d73
Victor Langa <victor.langa.delafuente@gmail.com>
2024-12-07 11:00:48 +0100

    Add color gradual change

--- Assets/Scripts/Meteorite/PathManager.cs
+++ Assets/Scripts/Meteorite/PathManager.cs
@@ -11,8 +11,9 @@ public class PathManager : MonoBehaviour
     [Space]
     [BoxGroup("Background values"), SerializeField] private Transform _backgroundTransform;
     [BoxGroup("Background values"), SerializeField] private float _backgroundTotalDisplacement;
-    [BoxGroup("Background values"), SerializeField] private Transform[] _mountains;
-    [BoxGroup("Background values"), SerializeField] private Transform _clouds;
+    [BoxGroup("Background values"), SerializeField] private SpriteRenderer[] _mountains;
+    [BoxGroup("Background values"), SerializeField] private SpriteRenderer _clouds;
+    [BoxGroup("Background values"), SerializeField] private Color _redColor;
 
     public void AddDisplacement(float currentSpeed, Vector2 minMaxSpeed, float dt)
     {
@@ -24,6 +25,7 @@ public class PathManager : MonoBehaviour
         _backgroundTransform.position = new(_backgroundTransform.position.x, _backgroundTotalDisplacement * (_currentDistance / _totalDistanceToTravel), _backgroundTransform.position.z);
 
         ApplyMountainDisplacement();
+        ApplyMountainColoring();
     }
 
     private void ApplyMountainDisplacement()
@@ -35,16 +37,34 @@ public class PathManager : MonoBehaviour
         if (parentPositionY > -30 && parentPositionY < 2.5)
         {
             float factor = Mathf.InverseLerp(-30, 2.5f, parentPositionY);
-            _clouds.localPosition = new(_clouds.localPosition.x, Mathf.Lerp(30.0f, 2.5f, factor), _clouds.localPosition.z);
+            _clouds.transform.localPosition = new(_clouds.transform.localPosition.x, Mathf.Lerp(30.0f, 2.5f, factor), _clouds.transform.localPosition.z);
         }
 
         if (parentPositionY > -20 && parentPositionY < 0)
         {
             float factor = Mathf.InverseLerp(-20, 0, parentPositionY);
 
-            _mountains[0].localPosition = new(_mountains[0].localPosition.x, Mathf.Lerp(12.5f, 0.0f, factor), _mountains[0].localPosition.z);
-            _mountains[1].localPosition = new(_mountains[1].localPosition.x, Mathf.Lerp(10.0f, 0.0f, factor), _mountains[1].localPosition.z);
-            _mountains[2].localPosition = new(_mountains[2].localPosition.x, Mathf.Lerp(5.0f, 0.0f, factor), _mountains[2].localPosition.z);
+            _mountains[0].transform.localPosition = new(_mountains[0].transform.localPosition.x, Mathf.Lerp(12.5f, 0.0f, factor), _mountains[0].transform.localPosition.z);
+            _mountains[1].transform.localPosition = new(_mountains[1].transform.localPosition.x, Mathf.Lerp(10.0f, 0.0f, factor), _mountains[1].transform.localPosition.z);
+            _mountains[2].transform.localPosition = new(_mountains[2].transform.localPosition.x, Mathf.Lerp(5.0f, 0.0f, factor), _mountains[2].transform.localPosition.z);
         }
+    }
+
+    private void ApplyMountainColoring()
+    {
+        // This is hardcoded
+
+        if (_clouds.transform.position.y > -10 && _clouds.transform.position.y < 2.5)
+        {
+            float factor = Mathf.InverseLerp(-10, 2.5f, _clouds.transform.position.y);
+            _clouds.color = Color.Lerp(Color.white, _redColor, factor);
+        }
+
+        foreach (var mountain in _mountains)
+            if (mountain.transform.position.y > -10 && mountain.transform.position.y < 5)
+            {
+                float factor = Mathf.InverseLerp(-10, 5, mountain.transform.position.y);
+                mountain.color = Color.Lerp(Color.white, _redColor, factor);
+            }
     }
 }
