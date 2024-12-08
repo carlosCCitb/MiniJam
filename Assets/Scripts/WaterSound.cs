a0154c69787f4777bd41a9c283967d2b3f546131
Victor Langa <victor.langa.delafuente@gmail.com>
2024-12-08 18:06:59 +0100

    Water sound separated

---
+++ Assets/Scripts/WaterSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+public class WaterSound : MonoBehaviour
+{
+    public static event Action PlayWaterSplash = delegate { };
+    private Collider2D _collider;
+
+    public void Initialize()
+    {
+        _collider.enabled = true;
+    }
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+        Initialize();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.attachedRigidbody.gameObject.layer == 8)
+        {
+            _collider.enabled = false;
+            PlayWaterSplash?.Invoke();
+        }
+    }
+}
