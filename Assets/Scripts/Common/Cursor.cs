340976209fb484ad457a168c174733fb969a2d60
Carlos Carrasco <carlos.carrasco@itb.cat>
2024-12-08 23:01:12 +0100

    Cursor To Test

--- Assets/Scripts/Common/Cursor.cs
+++ Assets/Scripts/Common/Cursor.cs
@@ -1,24 +1,31 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-
+using System.Collections.Generic;
+using System;
 public class Cursor : MonoBehaviour
 {
-    private SpriteRenderer _spriteRenderer;
-    [SerializeField] private Color _enemy, _noEnemy;
+    [SerializeField] private Color _enemy, _noEnemy, _currentCursor;
     [SerializeField] private int _enemyLayer;
     private void Awake()
     {
-        _spriteRenderer.GetComponent<SpriteRenderer>();
+        
     }
+    void OnGUI()
+    {
+        GUI.skin.settings.cursorColor = _currentCursor;
+    }
+
     private void Update()
     {
-        //Vector3 mousepos = Camera.main.Scre
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
-
-        // If it hits something...
+        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousepos.z = -10f;
+        RaycastHit2D hit = Physics2D.Raycast(mousepos, Vector3.forward);
         if (hit.collider.gameObject.layer == _enemyLayer)
         {
-
+            _currentCursor = Color.Lerp(_currentCursor, _enemy, 0.5f); 
+        }
+        else
+        {
+            _currentCursor = Color.Lerp(_currentCursor, _noEnemy, 0.5f);
         }
     }
 }
